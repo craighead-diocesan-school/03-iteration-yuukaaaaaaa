595362fc6d2156ea0846b02c5568d2f1cb6f132e
yuukaaaaaaa <sugiyayu93@craighead.school.nz>
2023-06-23 19:42:05 -0700

    completed task

--- code/5-ext-fibonacci.js
+++ code/5-ext-fibonacci.js
@@ -3,5 +3,10 @@
 // ####################################
 
 function fibonacci() {
-  // write your code here
+  let number = Number(prompt('What number do you want the Fibonacci Sequence up to?'))
+  nUmber = number + number 
+  nuMber = number + nUmber
+  numBer = nUmber + nuMber 
+  alert(number + ' , ' + number + ' , ' + nUmber + ' , ' + nuMber + ' , ' + numBer)
+
 }
